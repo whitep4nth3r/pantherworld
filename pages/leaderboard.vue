97ded930f3a191323d7cf980f1a076f417107f85
Ricardo de Vries <ricardozegt@gmail.com>
2024-07-26 14:44:25 +0200

    feat: Style the leaderboard a little

--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -28,30 +28,34 @@ const { user } = useUserSession();
 <template>
   <h1 class="text-4xl font-bold mb-12">Leaderboard</h1>
 
-  <section>
-    <table class="m-auto w-full table-auto text-left">
-      <thead>
+  <section class="overflow-x-auto">
+    <table class="min-w-full divide-y divide-gray-700">
+      <thead class="bg-gray-800">
         <tr>
-          <th scope="col" class="border border-slate-300 p-2">Rank</th>
-          <th scope="col" class="border border-slate-300 p-2">Username</th>
-          <th scope="col" class="border border-slate-300 p-2">Items</th>
-          <th scope="col" class="border border-slate-300 p-2">Wealth index</th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Rank</th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Username</th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Items</th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Wealth index</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody class="bg-black divide-y divide-gray-700">
         <tr
           v-for="(player, index) in data!.leaderboard.players"
+          :key="player.username"
           :class="{ 'bg-violet-700 text-white': user?.name === player.username }">
-          <td class="border border-slate-300 p-2">
-            {{ index + 1 }}
+          <td class="px-6 py-3 whitespace-nowrap text-gray-300">
+            <span v-if="index === 0">🥇</span>
+            <span v-else-if="index === 1">🥈</span>
+            <span v-else-if="index === 2">🥉</span>
+            <span v-else>{{ index + 1 }}</span>
           </td>
-          <td class="border border-slate-300 p-2">
+          <td class="px-6 py-3 whitespace-nowrap flex items-center text-gray-300">
             {{ player.username }}
           </td>
-          <td class="border border-slate-300 p-2">
+          <td class="px-6 py-3 whitespace-nowrap text-gray-300">
             {{ player.items }}
           </td>
-          <td class="border border-slate-300 p-2">
+          <td class="px-6 py-3 whitespace-nowrap text-gray-300">
             {{ player.wealth_index }}
           </td>
         </tr>
