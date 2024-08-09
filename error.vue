b013bac78acfc10584bfae6bad3dc2082c307b71
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-08-09 14:30:45 +0100

    fix 404 page title and description

--- error.vue
+++ error.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-const route = useRoute();
 const error = useError();
+
+useSeoMeta({
+  title: "404 | p4nth3rworld",
+  description: "404 Page not found",
+});
+
 useHead({
-  link: [
-    {
-      rel: "icon",
-      href: () =>
-        `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%25%22 y=%220.95em%22 text-anchor=%22middle%22 font-size=%2290px%22>${
-          route.meta.favicon || `🌍`
-        }</text></svg>`,
-    },
-  ],
   bodyAttrs: {
     class: "m-auto p-4 max-w-screen-md bg-zinc-900 text-zinc-100",
   },
