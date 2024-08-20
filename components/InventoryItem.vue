934c7583aba7b34ee3be4a79473217399d4d4c0a
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-08-20 15:02:06 +0100

    Try and make inventory look less chaotic

--- components/InventoryItem.vue
+++ components/InventoryItem.vue
@@ -13,7 +13,7 @@ const irradiated = computed(() => props.name.includes("irradiated"));
 
 <template>
   <div
-    class="flex flex-col flex-grow gap-1 bg-zinc-800 rounded-lg relative text-center p-4 bg-gradient-to-t via-10% to-zinc-800 to-60%"
+    class="grid grid-cols-[2fr_5fr] gap-4 bg-zinc-800 rounded-lg relative items-center p-4 bg-gradient-to-t via-10% to-zinc-800 to-60%"
     :class="{ 'from-lime-600 via-lime-800': irradiated }">
     <span v-if="displayCount" class="absolute text-white bg-zinc-600 rounded-xl py1 px-2 left-2 -top-2 italic font-bold"
       >{{ count }}x</span
@@ -28,14 +28,19 @@ const irradiated = computed(() => props.name.includes("irradiated"));
       class="absolute text-white bg-zinc-600 rounded-xl py1 px-2 left-2 -top-2 italic font-bold capitalize"
       >Cooked</span
     >
-    <span class="m-auto p-4 rounded-full bg-zinc-600 s-14 text-2xl leading-none">
+
+    <span class="justify-center w-[4rem] h-[4rem] p-4 rounded-full items-center flex bg-zinc-600 text-2xl leading-none">
       <template v-if="InventoryItemImages[name.replace(' ', '-')] !== undefined">
         {{ InventoryItemImages[name.replace(" ", "-")] }}
       </template>
 
+      <span v-else-if="cooked">🍳</span>
       <img v-else src="/icons/items/circle.svg" height="24" width="24" :alt="name" />
     </span>
-    <span class="text-white text-lg capitalize">{{ name }}</span>
-    <span class="text-white">{{ Intl.NumberFormat().format(rarity) }}</span>
+
+    <div>
+      <span class="text-white text-base capitalize mb-2 block">{{ name }}</span>
+      <span class="text-white block">{{ Intl.NumberFormat().format(rarity) }}</span>
+    </div>
   </div>
 </template>
