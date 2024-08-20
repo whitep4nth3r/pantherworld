6a445fd2bfd370fc36000a432e16ceec1447ed76
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-08-20 16:56:59 +0100

    Use UNotification for event feed stuff, still big WIP

--- components/EventFeed.vue
+++ components/EventFeed.vue
@@ -29,6 +29,7 @@ type Event = {
     giver?: string;
     receiver?: string;
   };
+  id: string;
   type: string;
 };
 
@@ -44,39 +45,79 @@ watch(data, (message) => {
 
 // limit to 3 events?
 // format events depending on "message.type?"
+// check for ws connection and reconnect if disconnects
+// decide on presentation
 </script>
 
 <template>
   <div>
     <h1>Event feed</h1>
     <div v-for="entry in history">
-      <p v-if="entry.type === 'scanned'">
-        👀 {{ entry.data.username }} scanned {{ entry.data.zone }} zone and found {{ entry.data.count }} items
-      </p>
-      <p v-if="entry.type === 'cookfail'">
-        🤮 {{ entry.data.username }} spoiled a recipe! They lost {{ entry.data.ingredientList?.join(", ") }}!
-      </p>
-      <p v-if="entry.type === 'cook'">
-        🍳 {{ entry.data.username }} cooked {{ entry.data.recipe }}
-        <span v-if="entry.data.lost && entry.data.lost?.length > 0">, but lost {{ entry.data.lost.join(", ") }}!</span>
-        (rarity: {{ entry.data.rarity }})
-      </p>
-      <p v-if="entry.type === 'release'">📦 {{ entry.data.count }} items released in the {{ entry.data.zone }} zone</p>
-      <p v-if="entry.type === 'move'">➡️ {{ entry.data.username }} moved to the {{ entry.data.toZone }} zone</p>
-      <p v-if="entry.type === 'claim'">
-        🎒 {{ entry.data.username }} claimed {{ entry.data.item }} in the {{ entry.data.zone }} zone
-      </p>
-      <p v-if="entry.type === 'drop'">
-        🫳 {{ entry.data.username }} dropped {{ entry.data.item }} in the {{ entry.data.zone }} zone (rarity:
-        {{ entry.data.rarity }})
-      </p>
-      <p v-if="entry.type === 'gift'">
-        🎁 {{ entry.data.giver }} gifted {{ entry.data.item }} to {{ entry.data.receiver }} in the
-        {{ entry.data.zone }} zone (rarity: {{ entry.data.rarity }})
-      </p>
-      <p v-if="entry.type === 'spawn' && entry.data.item !== undefined && typeof entry.data.item !== 'string'">
-        🌏 {{ entry.data.item.name }} spawned in the {{ entry.data.zone }} zone (rarity: {{ entry.data.item.rarity }})
-      </p>
+      <UNotification
+        v-if="entry.type === 'scanned'"
+        :description="`${entry.data.username} scanned ${entry.data.zone} zone and found ${entry.data.count} items`"
+        :id="entry.id"
+        :timeout="0"
+        title="👀 Scan" />
+
+      <UNotification
+        v-if="entry.type === 'cookfail'"
+        :description="`${entry.data.username} spoiled a recipe! They lost ${entry.data.ingredientList?.join(', ')}!`"
+        :id="entry.id"
+        :timeout="0"
+        title="🤮 Cook failed" />
+
+      <UNotification
+        v-if="entry.type === 'cook'"
+        :description="`${entry.data.username} cooked ${entry.data.recipe}${entry.data.lost && entry.data.lost?.length > 0 ? `, but lost ${entry.data.lost.join(', ')}}!` : ``} (rarity: ${entry.data.rarity})`"
+        :id="entry.id"
+        :timeout="0"
+        title="🍳 Cooked" />
+
+      <UNotification
+        v-if="entry.type === 'release'"
+        :description="`${entry.data.count} items released in the ${entry.data.zone} zone`"
+        :id="entry.id"
+        :timeout="0"
+        title="📦 Release" />
+
+      <UNotification
+        v-if="entry.type === 'move'"
+        :description="`${entry.data.username} moved to the ${entry.data.toZone} zone`"
+        :id="entry.id"
+        :timeout="0"
+        title="➡️ Move" />
+
+      <UNotification
+        v-if="entry.type === 'claim'"
+        :description="`${entry.data.username} claimed ${entry.data.item} in the ${entry.data.zone} zone (rarity:
+        ${entry.data.rarity})`"
+        :id="entry.id"
+        :timeout="0"
+        title="🎒 Claim" />
+
+      <UNotification
+        v-if="entry.type === 'drop'"
+        :description="`${entry.data.username} dropped ${entry.data.item} in the ${entry.data.zone} zone (rarity:
+        ${entry.data.rarity})`"
+        :id="entry.id"
+        :timeout="0"
+        title="🫳 Drop" />
+
+      <UNotification
+        v-if="entry.type === 'gift'"
+        :description="`${entry.data.giver} gifted ${entry.data.item} to ${entry.data.receiver} in the
+        ${entry.data.zone} zone (rarity: ${entry.data.rarity})`"
+        :id="entry.id"
+        :timeout="0"
+        title="🎁 Gift" />
+
+      <UNotification
+        v-if="entry.type === 'spawn' && entry.data.item !== undefined && typeof entry.data.item !== 'string'"
+        :description="`${entry.data.item.name} spawned in the ${entry.data.zone} zone (rarity: ${entry.data.item.rarity})`"
+        :id="entry.id"
+        :timeout="0"
+        title="🌏 Spawn" />
     </div>
   </div>
 </template>
