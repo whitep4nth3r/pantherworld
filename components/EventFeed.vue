f1cc7410ec2c8ac8e77c663f09afd310c0b5bcd1
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-08-21 14:39:40 +0100

    Notifs in progress

--- components/EventFeed.vue
+++ components/EventFeed.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { useWebSocket } from "@vueuse/core";
+const toast = useToast();
 
 const validEvents = [
   "itemspawn",
@@ -33,13 +34,136 @@ type Event = {
   type: string;
 };
 
-const history = ref<Event[]>([]);
+function getNotifTitle(event: Event): string {
+  switch (event.type) {
+    case "scanned":
+      return `${event.data.username} scanned ${event.data.zone} zone and found ${event.data.count} items`;
+    case "cookfail":
+      return `${event.data.username} spoiled a recipe! They lost ${event.data.ingredientList?.join(", ")}!`;
+    case "cook":
+      return `${event.data.username} cooked ${event.data.recipe}${event.data.lost && event.data.lost?.length > 0 ? `, but lost ${event.data.lost.join(", ")}}!` : ``} (rarity: ${event.data.rarity})`;
+    case "release":
+      return `${event.data.count} items released in the ${event.data.zone} zone`;
+    case "move":
+      return `${event.data.username} moved to the ${event.data.toZone} zone`;
+    case "claim":
+      return `${event.data.username} claimed ${event.data.item} in the ${event.data.zone} zone (rarity:
+        ${event.data.rarity})`;
+    case "drop":
+      return `${event.data.username} dropped ${event.data.item} in the ${event.data.zone} zone (rarity:
+        ${event.data.rarity})`;
+    case "gift":
+      return `${event.data.giver} gifted ${event.data.item} to ${event.data.receiver} in the
+        ${event.data.zone} zone (rarity: ${event.data.rarity})`;
+    case "spawn":
+      // @ts-ignore
+      return `${event.data.item.name} spawned in the ${event.data.zone} zone (rarity: ${event.data.item.rarity})`;
+    default:
+      return "";
+  }
+}
+
+function getNotifAvatar(type: string): string {
+  switch (type) {
+    case "scanned":
+      return "/icons/notifications/scanned.png";
+    case "cookfail":
+      return "/icons/notifications/cookfail.png";
+    case "cook":
+      return "/icons/notifications/cook.png";
+    case "release":
+      return "/icons/notifications/release.png";
+    case "move":
+      return "/icons/notifications/move.png";
+    case "claim":
+      return "/icons/notifications/claim.png";
+    case "drop":
+      return "/icons/notifications/drop.png";
+    case "gift":
+      return "/icons/notifications/gift.png";
+    case "spawn":
+      return "/icons/notifications/spawn.png";
+    default:
+      return "";
+  }
+}
+
+function getNotifColors(type: string): { background: string; text: string } {
+  switch (type) {
+    case "scanned":
+      return { background: "bg-notif-scanned dark:bg-notif-scanned", text: "text-notif-black dark:text-notif-black" };
+    case "cookfail":
+      return { background: "bg-notif-cookfail dark:bg-notif-cookfail", text: "text-notif-white dark:text-notif-white" };
+    case "cook":
+      return { background: "bg-notif-cook dark:bg-notif-cook", text: "text-notif-black dark:text-notif-black" };
+    case "release":
+      return { background: "bg-notif-release dark:bg-notif-release", text: "text-notif-white dark:text-notif-white" };
+    case "move":
+      return { background: "bg-notif-move dark:bg-notif-move", text: "text-notif-white dark:text-notif-white" };
+    case "claim":
+      return { background: "bg-notif-claim dark:bg-notif-claim", text: "text-notif-black dark:text-notif-black" };
+    case "drop":
+      return { background: "bg-notif-drop dark:bg-notif-drop", text: "text-notif-white dark:text-notif-white" };
+    case "gift":
+      return { background: "bg-notif-gift dark:bg-notif-gift", text: "text-notif-white dark:text-notif-white" };
+    case "spawn":
+      return { background: "bg-notif-spawn dark:bg-notif-spawn", text: "text-notif-white dark:text-notif-white" };
+    default:
+      return { background: "", text: "" };
+  }
+}
+
+function getNotifProgressBg(type: string): string {
+  switch (type) {
+    case "scanned":
+      return "bg-notif-black dark:bg-notif-black";
+    case "cookfail":
+      return "bg-notif-white dark:bg-notif-white";
+    case "cook":
+      return "bg-notif-black dark:bg-notif-black";
+    case "release":
+      return "bg-notif-white dark:bg-notif-white";
+    case "move":
+      return "bg-notif-white dark:bg-notif-white";
+    case "claim":
+      return "bg-notif-black dark:bg-notif-black";
+    case "drop":
+      return "bg-notif-white dark:bg-notif-white";
+    case "gift":
+      return "bg-notif-white dark:bg-notif-white";
+    case "spawn":
+      return "bg-notif-white dark:bg-notif-white";
+    default:
+      return "";
+  }
+}
 
 watch(data, (message) => {
   const messageObj = JSON.parse(message);
 
   if (validEvents.includes(messageObj.event)) {
-    history.value.push(messageObj);
+    const uiColours = getNotifColors(messageObj.type);
+
+    toast.add({
+      id: messageObj.id,
+      title: getNotifTitle(messageObj),
+      timeout: 6000,
+      avatar: { src: getNotifAvatar(messageObj.type), size: "sm" },
+      icon: "",
+      // @ts-ignore
+      closeButton: null,
+      ui: {
+        background: uiColours.background,
+        title: uiColours.text,
+        description: uiColours.text,
+        padding: "p-2",
+        // ring: "ring-2 ring-white dark:ring-white",
+        shadow: "shadow-xl",
+        progress: {
+          background: getNotifProgressBg(messageObj.type),
+        },
+      },
+    });
   }
 });
 
@@ -50,74 +174,5 @@ watch(data, (message) => {
 </script>
 
 <template>
-  <div>
-    <h1>Event feed</h1>
-    <div v-for="entry in history">
-      <UNotification
-        v-if="entry.type === 'scanned'"
-        :description="`${entry.data.username} scanned ${entry.data.zone} zone and found ${entry.data.count} items`"
-        :id="entry.id"
-        :timeout="0"
-        title="👀 Scan" />
-
-      <UNotification
-        v-if="entry.type === 'cookfail'"
-        :description="`${entry.data.username} spoiled a recipe! They lost ${entry.data.ingredientList?.join(', ')}!`"
-        :id="entry.id"
-        :timeout="0"
-        title="🤮 Cook failed" />
-
-      <UNotification
-        v-if="entry.type === 'cook'"
-        :description="`${entry.data.username} cooked ${entry.data.recipe}${entry.data.lost && entry.data.lost?.length > 0 ? `, but lost ${entry.data.lost.join(', ')}}!` : ``} (rarity: ${entry.data.rarity})`"
-        :id="entry.id"
-        :timeout="0"
-        title="🍳 Cooked" />
-
-      <UNotification
-        v-if="entry.type === 'release'"
-        :description="`${entry.data.count} items released in the ${entry.data.zone} zone`"
-        :id="entry.id"
-        :timeout="0"
-        title="📦 Release" />
-
-      <UNotification
-        v-if="entry.type === 'move'"
-        :description="`${entry.data.username} moved to the ${entry.data.toZone} zone`"
-        :id="entry.id"
-        :timeout="0"
-        title="➡️ Move" />
-
-      <UNotification
-        v-if="entry.type === 'claim'"
-        :description="`${entry.data.username} claimed ${entry.data.item} in the ${entry.data.zone} zone (rarity:
-        ${entry.data.rarity})`"
-        :id="entry.id"
-        :timeout="0"
-        title="🎒 Claim" />
-
-      <UNotification
-        v-if="entry.type === 'drop'"
-        :description="`${entry.data.username} dropped ${entry.data.item} in the ${entry.data.zone} zone (rarity:
-        ${entry.data.rarity})`"
-        :id="entry.id"
-        :timeout="0"
-        title="🫳 Drop" />
-
-      <UNotification
-        v-if="entry.type === 'gift'"
-        :description="`${entry.data.giver} gifted ${entry.data.item} to ${entry.data.receiver} in the
-        ${entry.data.zone} zone (rarity: ${entry.data.rarity})`"
-        :id="entry.id"
-        :timeout="0"
-        title="🎁 Gift" />
-
-      <UNotification
-        v-if="entry.type === 'spawn' && entry.data.item !== undefined && typeof entry.data.item !== 'string'"
-        :description="`${entry.data.item.name} spawned in the ${entry.data.zone} zone (rarity: ${entry.data.item.rarity})`"
-        :id="entry.id"
-        :timeout="0"
-        title="🌏 Spawn" />
-    </div>
-  </div>
+  <UNotifications />
 </template>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,21 @@
 module.exports = {
   content: ["./components/**/*.{js,vue,ts}", "./pages/**/*.vue", "./app.vue", "./error.vue"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "notif-black": "#0f111a",
+        "notif-white": "#ffffff",
+        "notif-scanned": "#38bdf8",
+        "notif-release": "#fc814a",
+        "notif-spawn": "#f0047f",
+        "notif-move": "#0e0016",
+        "notif-gift": "#fc814a",
+        "notif-cook": "#a3e635",
+        "notif-cookfail": "#dc2626",
+        "notif-drop": "#fc814a",
+        "notif-claim": "#ffffff",
+      },
+    },
   },
   plugins: [],
 };
