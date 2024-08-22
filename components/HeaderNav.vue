9ed68956519e0b18881d0754f34f61120fea852c
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-08-22 15:36:25 +0100

    Turn svg into svg files

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// @ts-ignore
 import { getData, setData } from "nuxt-storage/local-storage";
 import { onMounted } from "vue";
 import { onClickOutside } from "@vueuse/core";
@@ -42,34 +43,11 @@ onMounted(() => {
 <template>
   <button
     type="button"
-    class="notificationToggleButton flex gap-1 justify-between rounded-lg px-4 py-2 bottom-2 left-2 fixed focus:outline-none focus:ring focus:ring-emerald-300 active:ring active:ring-emerald-300 z-10 bg-zinc-700 shadow"
+    class="text-white notificationToggleButton flex gap-2 justify-between rounded-lg px-4 py-2 bottom-2 left-2 fixed focus:outline-none focus:ring focus:ring-emerald-300 active:ring active:ring-emerald-300 z-10 bg-zinc-700 shadow"
     @click="() => toggleNotifications()"
     v-bind:aria-label="showNotifications ? 'Turn off event feed in sidebar' : 'Turn on event feed in sidebar'">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="size-6"
-      v-if="showNotifications">
-      <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-      <path
-        fill-rule="evenodd"
-        d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
-        clip-rule="evenodd" />
-    </svg>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="size-6"
-      v-if="!showNotifications">
-      <path
-        d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
-      <path
-        d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z" />
-      <path
-        d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
-    </svg>
+    <img src="/icons/utils/eye_open.svg" height="24" width="24" alt="eye" v-if="showNotifications" />
+    <img src="/icons/utils/eye_slash.svg" height="24" width="24" alt="eye with a slash" v-if="!showNotifications" />
     <span>Event feed</span>
   </button>
   <header class="flex flex-col w-full items-center mb-6" ref="target">
